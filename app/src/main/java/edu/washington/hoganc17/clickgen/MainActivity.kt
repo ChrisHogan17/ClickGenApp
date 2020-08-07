9f55389f4f400e3135452919c7b9665c1cb526ab
Chris Hogan <chrishogan17@outlook.com>
2020-08-07 00:26:03 -0700

    Add ability to play MP3 file

--- app/src/main/java/edu/washington/hoganc17/clickgen/MainActivity.kt
+++ app/src/main/java/edu/washington/hoganc17/clickgen/MainActivity.kt
@@ -1,11 +1,34 @@
 package edu.washington.hoganc17.clickgen
 
+import android.media.MediaPlayer
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    private var mp: MediaPlayer? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        btnPlay.setOnClickListener {
+            if (mp == null) {
+                mp = MediaPlayer.create(this, R.raw.robots)
+            }
+            mp?.start()
+        }
+
+        btnPause.setOnClickListener {
+            mp?.pause()
+        }
+
+        btnStop.setOnClickListener {
+            mp?.let {
+                it.release()
+                mp = null
+            }
+        }
     }
 }
