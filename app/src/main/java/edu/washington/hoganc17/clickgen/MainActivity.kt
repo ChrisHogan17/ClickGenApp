4d14406485a040950bf76aecfc6deb929f7b28f3
Chris Hogan <chrishogan17@outlook.com>
2020-08-13 02:01:39 -0700

    Add comments for clarification of audio mixing

--- app/src/main/java/edu/washington/hoganc17/clickgen/MainActivity.kt
+++ app/src/main/java/edu/washington/hoganc17/clickgen/MainActivity.kt
@@ -188,7 +188,6 @@ class MainActivity : AppCompatActivity() {
 
 
 
-    // Code from: https://stackoverflow.com/questions/40929243/how-to-mix-two-wav-files-without-noise
     private fun mixSound(trackOne: InputStream, trackTwo: InputStream) {
 
         val w1 = Wave(trackOne)
@@ -200,8 +199,11 @@ class MainActivity : AppCompatActivity() {
         trackTwo.close()
 
 
+
         // 8 bit file + 8 bit file = 8 bit file
         /*
+        // Code from: https://stackoverflow.com/questions/40929243/how-to-mix-two-wav-files-without-noise
+
         val output = ByteArray(if (music1.size > music2.size) music2.size else music1.size)
 
         for (i in output.indices) {
@@ -213,22 +215,36 @@ class MainActivity : AppCompatActivity() {
         }
         */
 
+        // Our Code
         // 16 bit + 16 bit = 16 bit file
+
+        // Since each value in the sampleAmplitudes is 2 bytes
+        // Make output byte array the length of the longest array * 2
         val output = ByteArray(if (music1.size > music2.size) music2.size * 2 else music1.size * 2)
 
         var outputIndex = 0
         for (i in music1.indices) {
+            // Numbers borrowed from Stack Overflow Code
+            // Needs fine tuning as we don't really understand why the values are what they are
             val sample1 = music1[i] / 128.0f * 1.2f
             val sample2 = music2[i] / 250.0f
 
+            // Average the altitude of each sample into one value
             val mixed = (sample1 + sample2) / 2
+            // Convert to binaryString
             var mixedString = Integer.toBinaryString(mixed.toInt())
+
+            // Pad each value so they all have 16 bits
             mixedString = mixedString.padStart(16, '0')
 
+            // Trim off overflow bits
             mixedString = mixedString.substring(mixedString.length - 16)
+
+            // Separate 2 byte value into individual bytes
             val byte1 = mixedString.substring(0, 8)
             val byte2 = mixedString.substring(8, 16)
 
+            // Add bytes sequentially to output byte array
             output[outputIndex] = Integer.parseInt(byte1, 2).toByte()
             output[outputIndex + 1] = Integer.parseInt(byte2, 2).toByte()
             outputIndex += 2
