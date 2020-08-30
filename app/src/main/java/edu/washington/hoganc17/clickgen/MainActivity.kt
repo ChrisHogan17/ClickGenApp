82fd1051573bbfb494b1518a1dd1b65240a4e19b
Chris Hogan <chrishogan17@outlook.com>
2020-08-30 01:05:53 -0700

    Eli Changes

--- app/src/main/java/edu/washington/hoganc17/clickgen/MainActivity.kt
+++ app/src/main/java/edu/washington/hoganc17/clickgen/MainActivity.kt
@@ -57,7 +57,7 @@ class MainActivity : AppCompatActivity(), OnUploadListener {
         val clickSampleAmps = audioManager.generateClicktrack(times, sampleRate, clickFreq, clickDur, length)
         Log.i("ELI", clickSampleAmps.size.toString())
         
-        val mixedTracks = audioManager.mixAmplitudesSixteenBit(songSampleAmps, clickSampleAmps)
+        val mixedTracks = audioManager.mixAmplitudesSixteenBit(clickSampleAmps, clickSampleAmps)
 
         val bundle = Bundle()
         bundle.putByteArray(PlayerFragment.OUT_BYTES, mixedTracks)
