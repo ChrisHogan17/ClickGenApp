df50ea59bbc356cbf559abcb97d42b60e4b6f64b
EliGrosman <eligrosman1@gmail.com>
2020-08-30 01:52:36 -0700

    revert changes

--- app/src/main/java/edu/washington/hoganc17/clickgen/MainActivity.kt
+++ app/src/main/java/edu/washington/hoganc17/clickgen/MainActivity.kt
@@ -9,7 +9,6 @@ import edu.washington.hoganc17.clickgen.fragment.UploadFragment
 import edu.washington.hoganc17.clickgen.model.AudioManager
 import edu.washington.hoganc17.clickgen.model.AudioTrio
 import edu.washington.hoganc17.clickgen.model.OnUploadListener
-import java.io.InputStream
 
 
 // Code for this media player was created with help from the example at:
@@ -39,16 +38,24 @@ class MainActivity : AppCompatActivity(), OnUploadListener {
         }
     }
 
-    override fun onFileUploaded(songInputStream: InputStream, clickInputStream: InputStream) {
+    override fun onFileUploaded(trio: AudioTrio) {
         Log.i("HULK", "Activity")
+
+        val times = trio.beatsArray.toIntArray()
+        val sampleRate = trio.sr.toFloat()
+        val songInputStream = trio.inputStream
 
         val w1 = Wave(songInputStream)
         val songSampleAmps: ShortArray = w1.sampleAmplitudes
         songInputStream.close()
 
-        val w2 = Wave(clickInputStream)
-        val clickSampleAmps: ShortArray = w2.sampleAmplitudes
-        clickInputStream.close()
+        // times and sr are given by the server. click_freq and click_dur will be set by the user
+        val clickFreq = 880.0f
+        val clickDur = 0.5f
+        val length = songSampleAmps.size
+
+        val clickSampleAmps = audioManager.generateClicktrack(times, sampleRate, clickFreq, clickDur, length)
+        Log.i("ELI", clickSampleAmps.size.toString())
 
         val mixedTracks = audioManager.mixAmplitudesSixteenBit(songSampleAmps, clickSampleAmps)
 
